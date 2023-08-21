0fa19c6867aec44a147227867b2a20dd38d3c93c
Hugo Cozzani <113481573+hcozzani@users.noreply.github.com>
2023-08-20 23:22:57 -0300

    hugo
    bd 2

---
+++ cozzanimonedas_tp2.sql
@@ -0,0 +1,219 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: monedas
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auditoria_cuentas`
+--
+
+DROP TABLE IF EXISTS `auditoria_cuentas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auditoria_cuentas` (
+  `auditoriaId` int NOT NULL AUTO_INCREMENT,
+  `cuentasId` int NOT NULL,
+  `currencyId` int NOT NULL,
+  `amount` decimal(8,2) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `operacion` varchar(50) NOT NULL,
+  `fecha_hora` timestamp NULL DEFAULT NULL,
+  `user_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`auditoriaId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auditoria_cuentas`
+--
+
+LOCK TABLES `auditoria_cuentas` WRITE;
+/*!40000 ALTER TABLE `auditoria_cuentas` DISABLE KEYS */;
+INSERT INTO `auditoria_cuentas` VALUES (7,5,2,1000.00,'root@localhost','Agrego','2023-07-06 02:10:59','fortinero');
+/*!40000 ALTER TABLE `auditoria_cuentas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuentas`
+--
+
+DROP TABLE IF EXISTS `cuentas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuentas` (
+  `cuentasId` int NOT NULL AUTO_INCREMENT,
+  `currencyId` int NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `amount` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`cuentasId`),
+  UNIQUE KEY `usuarioMonedaUnicos` (`currencyId`,`user_name`),
+  KEY `user_name` (`user_name`),
+  CONSTRAINT `cuentas_ibfk_1` FOREIGN KEY (`currencyId`) REFERENCES `currency` (`currencyId`),
+  CONSTRAINT `cuentas_ibfk_2` FOREIGN KEY (`user_name`) REFERENCES `usuarios` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuentas`
+--
+
+LOCK TABLES `cuentas` WRITE;
+/*!40000 ALTER TABLE `cuentas` DISABLE KEYS */;
+INSERT INTO `cuentas` VALUES (2,1,'pepe',15000.00),(3,2,'tuerca',19.80),(5,2,'fortinero',1000.00);
+/*!40000 ALTER TABLE `cuentas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `cuentas_insertar` AFTER INSERT ON `cuentas` FOR EACH ROW insert into auditoria_cuentas (cuentasId, currencyId, amount, usuario, operacion, fecha_hora, user_name)
+values (new.cuentasId, new.currencyId, new.amount, USER(), 'Agrego', CURRENT_TIMESTAMP, new.user_name) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `cuentas_modificar` AFTER UPDATE ON `cuentas` FOR EACH ROW insert into auditoria_cuentas (cuentasId, currencyId, amount, usuario, operacion, fecha_hora, user_name)
+values (NEW.cuentasId, NEW.currencyId, NEW.amount, USER(), 'Modifico', CURRENT_TIMESTAMP, NEW.user_name) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `cuentas_eliminar` AFTER DELETE ON `cuentas` FOR EACH ROW insert into auditoria_cuentas (cuentasId, currencyId, amount, usuario, operacion, fecha_hora, user_name)
+values (OLD.cuentasId, OLD.currencyId, OLD.amount, 
+USER(), 'Elimino', CURRENT_TIMESTAMP, OLD.user_name) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `currency`
+--
+
+DROP TABLE IF EXISTS `currency`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `currency` (
+  `currencyId` int NOT NULL AUTO_INCREMENT,
+  `currencyCode` varchar(3) NOT NULL,
+  PRIMARY KEY (`currencyId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `currency`
+--
+
+LOCK TABLES `currency` WRITE;
+/*!40000 ALTER TABLE `currency` DISABLE KEYS */;
+INSERT INTO `currency` VALUES (1,'ARS'),(2,'USD');
+/*!40000 ALTER TABLE `currency` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trx`
+--
+
+DROP TABLE IF EXISTS `trx`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trx` (
+  `trxId` int NOT NULL AUTO_INCREMENT,
+  `cuentaOrig` int NOT NULL,
+  `cuentaDes` int NOT NULL,
+  `fecha_hora` datetime DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  `amountOrig` decimal(8,2) DEFAULT NULL,
+  `amountDest` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`trxId`),
+  KEY `cuentaOrig` (`cuentaOrig`),
+  KEY `cuentaDes` (`cuentaDes`),
+  CONSTRAINT `trx_ibfk_1` FOREIGN KEY (`cuentaOrig`) REFERENCES `cuentas` (`cuentasId`),
+  CONSTRAINT `trx_ibfk_2` FOREIGN KEY (`cuentaDes`) REFERENCES `cuentas` (`cuentasId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trx`
+--
+
+LOCK TABLES `trx` WRITE;
+/*!40000 ALTER TABLE `trx` DISABLE KEYS */;
+INSERT INTO `trx` VALUES (1,2,3,'2023-07-05 22:11:14',1,30000.00,35000.00),(2,3,2,'2023-07-05 22:13:09',0,10500.00,20000.00);
+/*!40000 ALTER TABLE `trx` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `user_name` varchar(50) NOT NULL,
+  `pwd` varchar(20) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES ('fortinero','ac21','Pedro','Castro'),('mar123','1234','Martha','Rodriguez'),('pepe','abcd','Jose','Garcia'),('tuerca','1221','Manuel','Radicce');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-05 23:15:06
